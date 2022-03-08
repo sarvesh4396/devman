d0c7a28cd23b9ae357b62d636525057deca26aab
sarvesh4396 <heysarvesh@pm.me>
2022-03-08 13:10:38 +0530

    dropping symbl integration

--- src/apis/endpoints.js
+++ src/apis/endpoints.js
@@ -5,7 +5,6 @@ const { read_settings } = require("../utils/util");
 const CODEX_URL = "https://api.openai.com/v1/engines/";
 const endpoints = {
   carbon: { carbon: "https://carbon.now.sh/" },
-  symbl: { name: "url", name1: "url" },
   codex: {
     completion: `${CODEX_URL}${read_settings().openAI.engine}/completions`,
     name1: "url",
@@ -18,7 +17,6 @@ const endpoints = {
 
 module.exports = {
   datamuse: endpoints.datamuse,
-  symbl: endpoints.symbl,
   codex: endpoints.codex,
   carbon: endpoints.carbon,
 };
--- src/apis/symbl.js
+++
@@ -1,1 +0,0 @@
-// Symbol.ai implementation
